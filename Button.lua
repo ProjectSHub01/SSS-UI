d8e47a2c169fd4c05711ee55141cf08ee2e0b880
ProjectSHub01 <oooiug27@gmail.com>
2024-12-27 11:59:03 +0700

    Create Button.lua

---
+++ Button.lua
@@ -0,0 +1,35 @@
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
+local Stroke = Instance.new("UIStroke")
+
+ScreenGui.Name = "ImageButton"
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Size = UDim2.new(0, 40, 0, 40)
+ImageButton.Image = "http://www.roblox.com/asset/?id=74099166547943"
+
+UICorner.Parent = ImageButton
+UICorner.CornerRadius = UDim.new(0, 100)
+Stroke.Parent = ImageButton
+Stroke.Color = Color3.fromRGB(150, 0, 0)
+Stroke.Thickness = 1.5
+
+local function UpdatePosition()
+    local screenSize = workspace.CurrentCamera.ViewportSize
+    ImageButton.Position = UDim2.new(0.5, 0, 0, -40)
+    ImageButton.AnchorPoint = Vector2.new(0.5, 0)
+end
+
+UpdatePosition()
+workspace.CurrentCamera:GetPropertyChangedSignal("ViewportSize"):Connect(UpdatePosition)
+
+ImageButton.MouseButton1Down:Connect(function()
+    local vim = game:GetService("VirtualInputManager")
+    vim:SendKeyEvent(true, 305, false, game)
+    vim:SendKeyEvent(false, 305, false, game)
+end)
